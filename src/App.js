556c33cbb47c39c6331a1c51718987e8a702f3cd
ranjeet patil <patilranjeet3699@gmail.com>
2020-06-26 14:49:38 +0530

    Implemented task component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react'
+import {ThemeProvider,Box} from '@chakra-ui/core'
 // import DashBoard from './Pages/DashBoard'
 // import HomePage from './Pages/HomePage'
-import Typography from 'typography'
-import deYoungTheme from 'typography-theme-de-young'
-
-const typography = new Typography(deYoungTheme)
-typography.injectStyles()
+// import Typography from 'typography'
+// import deYoungTheme from 'typography-theme-de-young'
+import  Task from './components/Task'
 
 export default function App() {
   return (
-    <div>
-      {/* <HomePage/>
-      <DashBoard/> */}
-      App js
-    </div>
+    <ThemeProvider> 
+      <Box fontFamily='Source Sans Pro' >
+      <Task/>
+      </Box> 
+    </ThemeProvider>
   )
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import Priority from './Priority';
+import { Box ,Badge,Avatar,AvatarGroup} from "@chakra-ui/core";
 //TODO:add svg icon addMember at + button
 export default function Task(props) {  
 
+  const property={
+    title:'This is task'
+  }
+
   return (
-      <div>
-      <Priority/>
-      <div>{props.title}</div> 
-      <div>+</div>
-      </div>
+    <Box  borderWidth="1px" rounded="lg" overflow="hidden" as="div" backgroundColor="gray.100" boxShadow="md" width="48">
+      <Box p="3">
+      <Badge rounded="full" px="2" variantColor="teal">
+            low
+      </Badge>
+        <Box
+          mt="3"
+          as="h4"
+          lineHeight="tight"
+          isTruncated
+        >
+          {property.title}
+        </Box>
+        <AvatarGroup size="sm" max={2} m="1">
+        <Avatar name="Ryan Florence" src="https://bit.ly/ryan-florence" />
+        <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />
+        <Avatar name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
+        <Avatar name="Prosper Otemuyiwa" src="https://bit.ly/prosper-baba" />
+        <Avatar name="Christian Nwamba" src="https://bit.ly/code-beast" />
+        <Avatar name="Christian Nwamba" src="https://bit.ly/code-beast" />
+        </AvatarGroup>
+      </Box>
+    </Box>
   )
 }
 
